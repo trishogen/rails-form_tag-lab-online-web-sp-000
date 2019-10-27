d5d518e5e13eedb95b6b1338be639fdabdc3c5aa
Trish Ogen <patriciaogen@gmail.com>
2019-10-27 21:57:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
 end
